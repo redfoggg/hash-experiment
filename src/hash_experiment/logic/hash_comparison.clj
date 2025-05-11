9be218d9eab5de74d0a46de1c794ac1103f3a178
David Ferrari <davidchristenes13@gmail.com>
2025-05-11 00:00:43 -0300

    feat: add line plot for hash comparison logic

--- src/hash_experiment/logic/hash_comparison.clj
+++ src/hash_experiment/logic/hash_comparison.clj
@@ -2,9 +2,19 @@
   (:require
    [hash-experiment.logic.computed-chaining :as logic.computed-chaining]
    [hash-experiment.logic.double-hashing :as logic.double-hashing]
-   [hash-experiment.logic.explicit-chaining :as logic.explicit-chaining]))
+   [hash-experiment.logic.explicit-chaining :as logic.explicit-chaining]
+   [incanter.charts :as charts]))
 
 (def m 997)
 (def trials 10)
 (def alpha-range (range 0.1 1.0 0.1))
+
+(def x-values (range 1 11))
+(def y-values (map #(* % %) x-values))
+
+(defn create-line-plot 
+  [x y]
+  (charts/view (charts/line-chart x y)))
+
+(create-line-plot x-values y-values)
 
