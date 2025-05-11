8eb73a728b6cc79d0bcdc03bedafbb8d40175f22
David Ferrari <davidchristenes13@gmail.com>
2025-05-11 00:35:16 -0300

    refactor: simplify and optimize hash functions and insert/search logic

--- src/hash_experiment/logic/computed_chaining.clj
+++ src/hash_experiment/logic/computed_chaining.clj
@@ -1,38 +1,34 @@
 (ns hash-experiment.logic.computed-chaining)
 
 (defn create-table
-  "Cria uma tabela vazia com m slots"
   [m]
   (vec (repeat m {:key nil :value nil :next nil})))
 
-(defn primary-hash
-  "Função hash primária"
+(defn hashing
   [k m]
   (mod k m))
 
-(defn secondary-hash
-  "Função hash secundária (nunca retorna zero)"
+(defn hashing-2
   [k m]
-  (inc (mod k (dec m))))
+  (mod (max 1 (quot k m)) m))
 
 (defn insert
-  "Insere um par chave-valor na tabela usando computed chaining"
-  [table k v]
+  [table k]
   (let [m (count table)
-        idx (primary-hash k m)
-        jump (secondary-hash k m)]
+        idx (hashing k m)
+        jump (hashing-2 k m)]
 
     (if (nil? (get-in table [idx :key]))
-      (assoc-in table [idx] {:key k :value v :next nil})
+      (assoc-in table [idx] {:key k :value k :next nil})
       (loop [current-idx idx
-             prev-idx nil
-             attempts 0]
+             prev-idx    nil
+             accesses    0]
         (cond
-          (>= attempts m) (throw (Exception. "Tabela cheia!"))
+          (>= accesses m) (throw (Exception. "Tabela cheia!"))
 
           (nil? (get-in table [current-idx :key]))
-          (let [new-idx current-idx
-                updated-table (assoc-in table [new-idx] {:key k :value v :next nil})]
+          (let [new-idx       current-idx
+                updated-table (assoc-in table [new-idx] {:key k :value k :next nil})]
             (if (= prev-idx nil)
               updated-table
               (assoc-in updated-table [prev-idx :next] new-idx)))
@@ -43,17 +39,16 @@
                    (if (= (get-in table [current-idx :next]) nil)
                      current-idx
                      prev-idx)
-                   (inc attempts))))))))
+                   (inc accesses))))))))
 
 (defn search
-  "Busca um valor na tabela pela chave"
   [table k]
   (let [m (count table)
-        initial-idx (primary-hash k m)]
+        initial-idx (hashing k m)]
     (loop [current-idx initial-idx
-           attempts 0]
+           accesses 0]
       (cond
-        (>= attempts m) nil
-        (= (get-in table [current-idx :key]) k) (get-in table [current-idx :value])
+        (>= accesses m) {:accesses accesses :value nil}
+        (= (get-in table [current-idx :key]) k) {:accesses accesses :value (get-in table [current-idx :value])}
         :else (recur (get-in table [current-idx :next])
-                     (inc attempts))))))
+                     (inc accesses))))))
