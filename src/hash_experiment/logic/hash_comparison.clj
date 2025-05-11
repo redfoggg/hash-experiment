3d0c915258579e459a3ec49590e8e50c6c2b6f65
David Ferrari <davidchristenes13@gmail.com>
2025-05-11 01:03:30 -0300

    refactor: rename and correct average-accesses function to average-success-failure-accesses

--- src/hash_experiment/logic/hash_comparison.clj
+++ src/hash_experiment/logic/hash_comparison.clj
@@ -10,22 +10,30 @@
 (def trials 10)
 (def alpha-range (range 0.1 1.0 0.1))
 
-(defn average-accesses [insert-fn search-fn alpha]
+(defn average-success-failure-accesses
+  [create-fn insert-fn search-fn alpha]
   (let [n (int (* alpha m))]
     (->> (range trials)
          (map (fn [_]
                 (let [keys (repeatedly n #(rand-int 10000))
-                      table (reduce insert-fn (insert-fn nil m) keys)
-                      accesses (map #(-> (search-fn table % m) :accesses) keys)]
-                  (/ (reduce + accesses) n))))
-         (reduce +)
-         (/ trials))))
+                      table (reduce insert-fn (create-fn m) keys)
+                      success-accesses (map #(-> (search-fn table %) :accesses) keys)
+                      fail-keys (repeatedly n #(rand-int 10000))
+                      fail-accesses (map #(-> (search-fn table %) :accesses) fail-keys)]
+                  {:success (/ (reduce + success-accesses) n)
+                   :failure (/ (reduce + fail-accesses) n)})))
+         (reduce (fn [acc x]
+                   {:success (+ (:success acc) (:success x))
+                    :failure (+ (:failure acc) (:failure x))})
+                 {:success 0 :failure 0})
+         (update :success #(/ % trials))
+         (update :failure #(/ % trials)))))
 
 (def xs alpha-range)
 
-(def ys-computed (map #(average-accesses logic.computed-chaining/insert logic.computed-chaining/search %) xs))
-(def ys-explicit (map #(average-accesses logic.explicit-chaining/insert logic.explicit-chaining/search %) xs))
-(def ys-double   (map #(average-accesses logic.double-hashing/insert logic.double-hashing/search %) xs))
+(def ys-computed (map #(average-success-failure-accesses logic.computed-chaining/insert logic.computed-chaining/search logic.computed-chaining/create-table %) xs))
+(def ys-explicit (map #(average-success-failure-accesses logic.explicit-chaining/insert logic.explicit-chaining/search logic.explicit-chaining/create-table %) xs))
+(def ys-double   (map #(average-success-failure-accesses logic.double-hashing/insert logic.double-hashing/search logic.double-hashing/create-table %) xs))
 
 (def chart (charts/xy-plot xs ys-computed
                            :title "Average Accesses vs Load Factor"
